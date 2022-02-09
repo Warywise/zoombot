75bf262d76bbd5b549cdbdd393424d690c29183d
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-02-09 16:44:28 -0300

    Torna o link a ser clicado, dinamico

--- src/components/Reminder.jsx
+++ src/components/Reminder.jsx
@@ -5,15 +5,26 @@ import Redirector from './Redirector';
 export default function Reminder() {
   const { time } = useContext(TimerContext);
   const [meetingTime, setMeetingTime] = useState();
+  const [meetingLink, setMeetingLink] = useState();
   return (
     <form>
+      {'Time: '}
       <input
         type="text"
         onChange={ ({ target }) => setMeetingTime(target.value) }
       />
+      <hr />
+      <br />
+      {'Link: '}
+      <input
+        type="text"
+        onChange={ ({ target }) => setMeetingLink(target.value) }
+      />
+      <hr />
+      <br />
       <Redirector
         redirect={ time === meetingTime }
-        url="https://trybe.zoom.us/j/94275512829"
+        url={ meetingLink }
       />
     </form>
   );
